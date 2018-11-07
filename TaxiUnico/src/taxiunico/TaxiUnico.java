280d5b8011072fa98743db3011b1fb5963768bc6
Gabriel Carrion <gabo_c_v@hotmail.com>
2018-11-06 18:12:30 -0600

    Creando JPanel para entradas de texto de registro

--- TaxiUnico/src/taxiunico/TaxiUnico.java
+++ TaxiUnico/src/taxiunico/TaxiUnico.java
@@ -5,13 +5,18 @@
  */
 package taxiunico;
 
+import java.awt.BorderLayout;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  *
@@ -21,22 +26,65 @@ public class TaxiUnico extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+        JLabel id = new JLabel ("id");
+        JLabel mail = new JLabel ("email");
+        JLabel pass = new JLabel ("contrasena");
+        JLabel nom = new JLabel("Nombre");
+        JLabel ape = new JLabel ("Apellido");
+        JLabel tel = new JLabel ("Telefono");
+        
+        JTextField inId = new JTextField ("ingresa id");
+        JTextField inMa = new JTextField ("ingresa mail");
+        JTextField inPa = new JTextField ("ingresa una contraseña");
+        JTextField inNom = new JTextField ("ingresa tu nombre");
+        JTextField inApe = new JTextField ("ingresa tu apellido");
+        JTextField inTel = new JTextField ("ingresa tu numero de telefono");
+        
+        JPanel paId = new JPanel(new BorderLayout());
+        paId.add(id,BorderLayout.WEST);
+        paId.add(inId,BorderLayout.CENTER);
+        
+        JPanel paMa = new JPanel(new BorderLayout());
+        paMa.add(mail,BorderLayout.WEST);
+        paMa.add(inMa,BorderLayout.CENTER);
+        
+        JPanel paPsw = new JPanel(new BorderLayout());
+        paPsw.add(pass,BorderLayout.WEST);
+        paPsw.add(inPa,BorderLayout.CENTER);
+        
+        JPanel paNom = new JPanel(new BorderLayout());
+        paNom.add(nom,BorderLayout.WEST);
+        paNom.add(inNom,BorderLayout.CENTER);
+        
+        JPanel paApe = new JPanel(new BorderLayout());
+        paApe.add(id,BorderLayout.WEST);
+        paApe.add(inId,BorderLayout.CENTER);
+        
+        
+        JPanel paTel = new JPanel(new BorderLayout());
+        paTel.add(tel,BorderLayout.WEST);
+        paTel.add(inTel,BorderLayout.CENTER);
+        
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("Sign in");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("registrate");
             }
+        
         });
+        
+       
+        
         
         StackPane root = new StackPane();
         root.getChildren().add(btn);
         
         Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Transpaís Taxi");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
