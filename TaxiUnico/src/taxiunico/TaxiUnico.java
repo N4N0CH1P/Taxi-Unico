2ebfdbac0faae7e4808abcdb12219bed563a10ab
Gabriel Carrion <gabo_c_v@hotmail.com>
2018-11-06 18:51:08 -0600

    Botones con javafx

--- TaxiUnico/src/taxiunico/TaxiUnico.java
+++ TaxiUnico/src/taxiunico/TaxiUnico.java
@@ -7,11 +7,16 @@ package taxiunico;
 
 import java.awt.BorderLayout;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javax.swing.JLabel;
@@ -26,47 +31,42 @@ public class TaxiUnico extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        JLabel id = new JLabel ("id");
-        JLabel mail = new JLabel ("email");
-        JLabel pass = new JLabel ("contrasena");
-        JLabel nom = new JLabel("Nombre");
-        JLabel ape = new JLabel ("Apellido");
-        JLabel tel = new JLabel ("Telefono");
+        HBox idBox = new HBox();
+        HBox passBox = new HBox();
         
-        JTextField inId = new JTextField ("ingresa id");
-        JTextField inMa = new JTextField ("ingresa mail");
-        JTextField inPa = new JTextField ("ingresa una contraseña");
-        JTextField inNom = new JTextField ("ingresa tu nombre");
-        JTextField inApe = new JTextField ("ingresa tu apellido");
-        JTextField inTel = new JTextField ("ingresa tu numero de telefono");
+        HBox password = new HBox();
+        idBox.setSpacing(10);
+        passBox.setSpacing(10);
         
-        JPanel paId = new JPanel(new BorderLayout());
-        paId.add(id,BorderLayout.WEST);
-        paId.add(inId,BorderLayout.CENTER);
+        Label id = new Label ("id");
+        Label psw = new Label ("contrasena");
+        Label mail = new Label ("mail");
+        Label nombre = new Label ("contrasena");
+        Label apellido = new Label ("apellido");
+        Label telefono = new Label ("telefono");
         
-        JPanel paMa = new JPanel(new BorderLayout());
-        paMa.add(mail,BorderLayout.WEST);
-        paMa.add(inMa,BorderLayout.CENTER);
+        TextField iden = new TextField ();
+        TextField tfP = new TextField ();
+        BorderPane Tapp = new BorderPane();
         
-        JPanel paPsw = new JPanel(new BorderLayout());
-        paPsw.add(pass,BorderLayout.WEST);
-        paPsw.add(inPa,BorderLayout.CENTER);
+        idBox.getChildren().addAll(id, iden);
+        passBox.getChildren().addAll(psw, tfP);
+       
+       HBox areaBotones = new HBox();
+       
+       
+       Button re = new Button("Registrate");
+       Button li = new Button("Sign In");
+       Button lo = new Button("Log out");
+      areaBotones.getChildren().addAll(re, li, lo);
+       
+        Tapp.setTop(idBox);
+        Tapp.setTop(passBox);
+        Tapp.setCenter(areaBotones);
         
-        JPanel paNom = new JPanel(new BorderLayout());
-        paNom.add(nom,BorderLayout.WEST);
-        paNom.add(inNom,BorderLayout.CENTER);
-        
-        JPanel paApe = new JPanel(new BorderLayout());
-        paApe.add(id,BorderLayout.WEST);
-        paApe.add(inId,BorderLayout.CENTER);
-        
-        
-        JPanel paTel = new JPanel(new BorderLayout());
-        paTel.add(tel,BorderLayout.WEST);
-        paTel.add(inTel,BorderLayout.CENTER);
         
         Button btn = new Button();
-        btn.setText("Sign in");
+        btn.setText("registrate");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
